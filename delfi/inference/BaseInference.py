a66d1971c22c0097449fd0c51c256371a1b9a0d8
kaandocal <kaandocal@uni-bonn.de>
2018-07-23 14:16:23 +0200

    Fixed MPGenerator issues (queue not closing)

--- delfi/inference/BaseInference.py
+++ delfi/inference/BaseInference.py
@@ -5,11 +5,13 @@ from delfi.neuralnet.NeuralNet import NeuralNet
 from delfi.neuralnet.Trainer import Trainer
 from delfi.utils.meta import ABCMetaDoc
 
+import theano
+dtype = theano.config.floatX
 
 class BaseInference(metaclass=ABCMetaDoc):
     def __init__(self, generator, 
-                 prior_norm=True,  
-                 pilot_samples=100,
+                 prior_norm=True, init_norm=False,
+                 pilot_samples=100, reinit_weights=False, 
                  seed=None, verbose=True, **kwargs):
         """Abstract base class for inference algorithms
 
@@ -49,6 +51,8 @@ class BaseInference(metaclass=ABCMetaDoc):
         self.generator = generator
         self.generator.proposal = None
 
+        self.reinit_weights = reinit_weights
+
         # generate a sample to get input and output dimensions
         params, stats = generator.gen(1, skip_feedback=True, verbose=False)
         kwargs.update({'n_inputs': stats.shape[1:],
@@ -78,6 +82,11 @@ class BaseInference(metaclass=ABCMetaDoc):
             self.stats_mean = np.zeros((stats.shape[1],))
             self.stats_std = np.ones((stats.shape[1],))
 
+        # optional: z-transform output for obs (also re-centres x onto obs!)
+        self.init_norm = init_norm
+        self.init_fcv = 0.8 if self.network.n_components > 1 else 0.
+        self.norm_init()            
+
         # observables contains vars that can be monitored during training
         self.compile_observables()
 
@@ -93,8 +102,13 @@ class BaseInference(metaclass=ABCMetaDoc):
         """Reinitializes the network instance (re-setting the weights!) 
 
         """
-        self.network = NeuralNet(**self.kwargs)
-        self.svi = self.network.svi
+        if self.reinit_weights:
+            print('re-initializing network weights')
+            self.network = NeuralNet(**self.kwargs)
+            self.svi = self.network.svi
+
+            self.norm_init()
+
 
     def centre_on_obs(self):
         """ Centres first-layer input onto observed summary statistics 
@@ -102,7 +116,7 @@ class BaseInference(metaclass=ABCMetaDoc):
         Ensures x' = x - xo, i.e. first-layer input x' = 0 for x = xo.
         """
 
-        self.stats_mean = self.obs.flatten()
+        self.stats_mean = self.obs.copy()
 
     def remove_hidden_biases(self):
         """ Resets all bias weights in hidden layers to zero.
@@ -114,8 +128,8 @@ class BaseInference(metaclass=ABCMetaDoc):
         for b in filter(idx_hiddens, self.network.mps_bp):
             b.set_value(np.zeros_like(b.get_value()))
 
-    def conditional_norm(self, fcv = 0.8):
-        """ Normalizes current network output at obersved summary statistics
+    def conditional_norm(self, fcv = 0.8, tmu=None, tSig=None, h=None):
+        """ Normalizes current network output at observed summary statistics
 
 
         Parameters
@@ -124,34 +138,85 @@ class BaseInference(metaclass=ABCMetaDoc):
             Fraction of total that comes from uncertainty over components, i.e.
             Var[th] = E[Var[th|z]] + Var[E[th|z]]
                     =  (1-fcv)     +     fcv       = 1
+        tmu: array
+            Target mean. 
+        tSig: array
+            Target covariance. 
+
         """
-        mog = self.predict(self.obs)
+
+        # avoiding CDELFI.predict() attempt to analytically correct for proposal
+        print('obs', self.obs.shape)
+        print('mean', self.stats_mean.shape)
+        print('std', self.stats_std.shape)
+        obz = (self.obs - self.stats_mean) / self.stats_std
+        posterior = self.network.get_mog(obz.reshape(self.obs.shape), deterministic=True)
+        mog =  posterior.ztrans_inv(self.params_mean, self.params_std)
 
         assert np.all(np.diff(mog.a)==0.) # assumes uniform alpha
 
-        assert len(mog.xs) == 2 # currently only valid for 2 components
-        Z1inv = np.sqrt((1.-fcv)*2./(mog.xs[0].S + mog.xs[1].S)).reshape(-1)
-        Z2inv = np.sqrt(  fcv   *4./(mog.xs[0].m - mog.xs[1].m)**2).reshape(-1)
+        n_dim = self.kwargs['n_outputs']
+        triu_mask = np.triu(np.ones([n_dim, n_dim], dtype=dtype), 1)
+        diag_mask = np.eye(n_dim, dtype=dtype)
 
+        # compute MoG mean mu, Sig = E[Var[th|z]] and C = Var[E[th|z]]
+        mu, Sig = np.zeros_like(mog.xs[0].m), np.zeros_like(mog.xs[0].S)
+        for i in range(self.network.n_components):
+            Sig += mog.a[i] * mog.xs[i].S
+            mu  += mog.a[i] * mog.xs[i].m
+        C = np.zeros_like(Sig)
+        for i in range(self.network.n_components):
+            dmu = mog.xs[i].m - mu if self.network.n_components > 1 else mog.xs[i].m
+            C   += mog.a[i] * np.outer(dmu, dmu)
+
+        # if not provied, target zero-mean unit variance (as for prior_norm=True)
+        tmu = np.zeros_like(mog.xs[0].m) if tmu is None else tmu
+        tSig = np.eye(mog.xs[0].m.size) if tSig is None else tSig
+
+        # compute normalizers (we only z-score, don't whiten!)
+        Z1inv = np.sqrt((1.-fcv) / np.diag(Sig) * np.diag(tSig)).reshape(-1)
+        Z2inv = np.sqrt(  fcv    / np.diag( C ) * np.diag(tSig)).reshape(-1)
+        
+        # first we need the center of means
         def idx_MoG(x):
             return x.name[:5]=='means'
-
-        # first we need the center of means
-        mu = np.zeros_like(mog.xs[0].m)
-        for b in filter(idx_MoG, self.network.mps_bp):
-            mu += b.get_value()
-        mu /= self.network.n_components
+        mu_ = np.zeros_like(mog.xs[0].m)
+        for w, b in zip(filter(idx_MoG, self.network.mps_wp),
+                        filter(idx_MoG, self.network.mps_bp)):                        
+            h = np.zeros(w.get_value().shape[0]) if h is None else h 
+            mu_ += h.dot(w.get_value()) + b.get_value()        
+        mu_ /= self.network.n_components
 
         # center and normalize means
-        for b in filter(idx_MoG, self.network.mps_bp):
-            b.set_value(Z2inv * (b.get_value() - mu))
+        # mu =  Z2inv * (Wh + b - mu_) + tmu
+        #    = Wh + (Z2inv * (b - mu_ + Wh) - Wh + tum) 
+        for w, b in zip(filter(idx_MoG, self.network.mps_wp),
+                        filter(idx_MoG, self.network.mps_bp)):
+            Wh = h.dot(w.get_value())
+            b.set_value(Z2inv * (Wh + b.get_value() - mu_) - Wh + tmu)
 
         # normalize covariances
         def idx_MoG(x):
             return x.name[:10]=='precisions'
-        for b in filter(idx_MoG, self.network.mps_bp):
-            b.set_value(b.get_value() - np.log(Z1inv))
+        # Sig^-0.5 = diag_mask * (exp(Wh+b)/exp(log(Z1)) + triu_mask * (Wh+b)*Z1
+        #          = diag_mask *  exp(Wh+ (b-log(Z1))    + triu_mask * (Wh+((b+Wh)*Z1-Wh))
+        for w, b in zip(filter(idx_MoG, self.network.mps_wp),
+                        filter(idx_MoG, self.network.mps_bp)):
+            Wh = h.dot(w.get_value()).reshape(n_dim,n_dim)
+            b_ = b.get_value().copy().reshape(n_dim,n_dim)
+            
+            val = diag_mask * (b_ - np.diag(np.log(Z1inv))) + triu_mask * ((b_+Wh).dot(np.diag(1./Z1inv))- Wh )
+            
+            b.set_value(val.flatten())
+            
 
+    def norm_init(self): 
+        if self.init_norm:
+            print('standardizing network initialization')
+            if self.network.n_components > 1:
+                self.standardize_init(fcv = self.init_fcv)
+            else:
+                self.standardize_init(fcv = 0.)
 
     def standardize_init(self, fcv = 0.8):
         """ Standardizes the network initialization on obs
@@ -202,6 +267,7 @@ class BaseInference(metaclass=ABCMetaDoc):
     def pilot_run(self, n_samples):
         """Pilot run in order to find parameters for z-scoring stats
         """
+
         verbose = '(pilot run) ' if self.verbose else False
         params, stats = self.generator.gen(n_samples, verbose=verbose)
         self.stats_mean = np.nanmean(stats, axis=0)
